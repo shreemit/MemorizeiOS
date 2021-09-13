22203f575816112d43c751a3e04200a1c228cff5
Shreemit <sreemith.s@humanfractal.io>
2021-09-13 15:54:41 +0530

    Initial Commit

---
+++ Memorize2/Memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2App.swift
+//  Memorize2
+//
+//  Created by Shreemit on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
