8bebe6ec046cd2a210fd4160d228fd2532971d70
Shreemit <sreemith.s@humanfractal.io>
2021-09-13 19:01:14 +0530

    Stanford Lecture 2 completed Using VStacks and HStacks to build interfaces

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["😂", "😅", "😇", "☹️", "😱", "😃", "😴", "🥶","🙄","😈","😵","😜","😷"]
+    @State var emojiCount = 4
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "minus.circle")
+            }
+        }
+    }
+
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            VStack {
+                Image(systemName: "plus.circle")
+            }
+        }
+    }
+
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+                    ForEach(emojis[0 ..< emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2 / 3, contentMode: .fit)
+                    }
+                }.foregroundColor(.red)
+            }
+            Spacer(minLength: 20)
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content: String
+    let shape = RoundedRectangle(cornerRadius: 25)
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 42)
-                .stroke()
-            Text("Placeholder")
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill().foregroundColor(.red)
+            }
         }
-            .padding()
-            .foregroundColor(.yellow)
+        .padding()
+        .foregroundColor(.red)
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
@@ -23,6 +82,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
