f4ca9669f8b0299ab79861dfc96936f973e0f74d
Shreemit <sreemith.s@humanfractal.io>
2021-09-13 16:59:15 +0530

    Initial Commit

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return RoundedRectangle(cornerRadius: 42).stroke().padding(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
